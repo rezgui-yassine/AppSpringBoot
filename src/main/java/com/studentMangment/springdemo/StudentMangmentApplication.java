8b4733918ebb3bbaeee7b480a5696f5358ef99d6
RezguiYassine07 <rezguiyassine224@gmail.com>
2023-08-07 12:19:09 +0200

    first commit

---
+++ src/main/java/com/studentMangment/springdemo/StudentMangmentApplication.java
@@ -0,0 +1,14 @@
+package com.studentMangment.springdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMangmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMangmentApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/studentMangment/springdemo/StudentMangmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentMangment.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMangmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
